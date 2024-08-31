963e51a9e5cc42856e7c7ee00d06d001c2634d53
Laurent Fenu <laurentfenu.devweb@gmail.com>
2024-08-31 15:44:21 +0200

    Valid authentication

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -9,8 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 
@@ -24,25 +22,25 @@ class AuthController extends AbstractController
     }
 
     #[Route('/auth', name: 'app_auth', methods: ['POST'])]
-    public function auth(User $user, SerializerInterface $serializer, Request $request, UserRepository $userRepository): Response
+    public function auth(SerializerInterface $serializer, Request $request, UserRepository $userRepository): Response 
     {
-        $user = $serializer->deserialize($request->getContent(), User::class,"json");
+        $userRequest = $serializer->deserialize($request->getContent(), User::class, "json");
+    
+        $storedUser = $userRepository->findOneBy(['username' => $userRequest->getUsername()]);
 
-        $user = $userRepository->findOneBy(['username' => $user->getUsername(), 'password' => $user->getPassword()]);
-        // var_dump($user);
-
-        if (!$user) {
-            // Si l'utilisateur n'est pas trouvé dans la base de données, renvoyer une réponse d'erreur
+        if (!$storedUser || !password_verify($userRequest->getPassword(), $storedUser->getPassword())) {
             return new JsonResponse(['message' => 'Identifiants invalides'], Response::HTTP_UNAUTHORIZED);
         }
-        
+
         // Générer un token JWT valide
         $token = $this->jwtEncoder->encode([
-            'username' => $user->getUsername(),
-            'exp' => time() + 3600, // expiration du token (ici 1 heure)
+            'username' => $storedUser->getUsername(),
+            'exp' => time() + 7200,
         ]);
-
-        // Retourner le token JWT encodé en JSON
-        return $this->json(['token' => $token]);
+    
+        return $this->json([
+            'token' => $token,
+            'Utilisateur connecté' => $storedUser->getUsername().' de la ville de '.$storedUser->getCity(),
+        ]);
     }
 }
